c4f172211bf4492359c2ad2da2def7d618c0046c
Undiy <undiy@yandex.ru>
2023-12-07 10:52:14 +0500

    Renamed preferences

--- gAInClicker/app/src/main/java/com/example/android/gainclicker/ServiceLocator.kt
+++ gAInClicker/app/src/main/java/com/example/android/gainclicker/ServiceLocator.kt
@@ -7,9 +7,9 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.example.android.gainclicker.data.realm.RealmGameStateRepository
 import com.example.android.gainclicker.settings.DataStoreSettingsRepository
 
-private const val LAYOUT_PREFERENCE_NAME = "layout_preferences"
+private const val PREFERENCES_NAME = "preferences"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-    name = LAYOUT_PREFERENCE_NAME
+    name = PREFERENCES_NAME
 )
 
 class ServiceLocator(context: Context) {
