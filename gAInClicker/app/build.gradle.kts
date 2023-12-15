3841d8a067698925d48376b65ceba83cae1087cb
Undiy <undiy@yandex.ru>
2023-12-15 15:37:08 +0500

    Migrate build to version catalogs

--- gAInClicker/app/build.gradle.kts
+++ gAInClicker/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("io.realm.kotlin")
-    id("io.github.skeptick.libres")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.gradlePlugin)
+    alias(libs.plugins.realm.gradlePlugin)
+    id(libs.plugins.libres.gradlePlugin.get().pluginId)
 }
 
 android {
@@ -53,29 +53,30 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("io.realm.kotlin:library-base:1.11.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("io.insert-koin:koin-androidx-compose:${rootProject.extra["koin_version"]}")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
 
-    testImplementation("junit:junit:4.13.2")
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.realm.base)
+    implementation(libs.datastore.preferences)
+    implementation(libs.koin.androidx.compose)
+
+    testImplementation(libs.junit4)
+
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test)
+
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.tooling.preview)
 }
 
 libres {
--- gAInClicker/build.gradle.kts
+++ gAInClicker/build.gradle.kts
@@ -1,17 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("io.realm.kotlin") version "1.11.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.gradlePlugin) apply false
+    alias(libs.plugins.realm.gradlePlugin) apply false
 }
 
 buildscript {
-    extra.apply {
-        set("lifecycle_version", "2.7.0-rc01")
-        set("libres_version", "1.2.2")
-        set("koin_version", "3.4.0")
-    }
     dependencies {
-        classpath("io.github.skeptick.libres:gradle-plugin:${rootProject.extra["libres_version"]}")
+        classpath(libs.libres)
     }
 }
