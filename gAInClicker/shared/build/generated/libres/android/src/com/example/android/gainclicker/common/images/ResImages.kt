f377a69d6be64ff6bd0d51639519a8ba6bc83571
Undiy <undiy@yandex.ru>
2023-12-20 14:52:43 +0500

    Remove build folder

--- gAInClicker/shared/build/generated/libres/android/src/com/example/android/gainclicker/common/images/ResImages.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android.gainclicker.common.images
-
-import com.example.android.gainclicker.common.R
-import io.github.skeptick.libres.images.Image
-
-public actual object ResImages
--- gAInClicker/shared/build/generated/libres/common/src/com/example/android/gainclicker/common/images/ResImages.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.gainclicker.common.images
-
-import io.github.skeptick.libres.images.Image
-
-public expect object ResImages
